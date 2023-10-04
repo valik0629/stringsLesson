788e23c8dc99f128846fd0655e9a1ded4ddab220
Valik <valik0629@gmail.com>
2023-10-04 22:15:29 +0300

    Finish commit, homework branch;

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("Task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника - " + fullName);
+
+
+        System.out.println("Task 2");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+
+        System.out.println("Task 3");
+        String fullName2 = "Иванов Семён Семёнович";
+        fullName2 = fullName2.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника - " + fullName2);
     }
 }
